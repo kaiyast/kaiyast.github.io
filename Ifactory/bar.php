ff364c1dc10c2db4135688d51bad8fee95568e12
Gai lowvapong <0kaiyast0@gmail.com>
2017-04-16 19:28:08 +0700

    add new web ifac

---
+++ Ifactory/bar.php
@@ -0,0 +1,43 @@
+          <!-- start:Left Menu -->
+            <div id="left-menu">
+              <div class="sub-left-menu scroll">
+                <ul class="nav nav-list">
+                    <li><div class="left-bg"></div></li>
+                    <li class="time">
+                      <h1 class="animated fadeInLeft">21:00</h1>
+                      <p class="animated fadeInRight">Sat,October 1st 2029</p>
+                    </li>
+                    <li class="ripple">
+                      <a class="tree-toggle nav-header"><span class="fa-wrench  fa"></span> Optimize 
+                        <span class="fa-angle-right fa right-arrow text-right"></span>
+                      </a>
+                    </li>
+                    <li class="ripple">
+                      <a class="tree-toggle nav-header"><span class="fa-users fa"></span> Worker 
+                        <span class="fa-angle-right fa right-arrow text-right"></span>
+                      </a>
+                    </li>
+                    <li class="ripple">
+                      <a class="tree-toggle nav-header">
+                       <span class="fa fa-shopping-cart " href="index.php" ></span> Product
+                        <span class="fa-angle-right fa right-arrow text-right"></span>
+                      </a>
+                    </li>
+                    <li class="ripple">
+                      <a class="tree-toggle nav-header" href="index.php">
+                        <span class="fa-area-chart fa"></span> Chart
+                        <span class="fa-angle-right fa right-arrow text-right"></span>
+                      </a>
+                    </li>
+                  
+                    <li><a href="index.php" >Logout</a></li>
+                  </ul>
+                </div>
+            </div>
+          <!-- end: Left Menu -->
+		  
+<script type="text/javascript">
+
+	
+
+</script>
---
+++ Ifactory/home.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	
+<?php include('header.php'); ?>
+	
+  </head>
+
+ <body id="mimin" class="dashboard">
+      <!-- start: Header -->
+        <nav class="navbar navbar-default header navbar-fixed-top">
+          <div class="col-md-12 nav-wrapper">
+            <div class="navbar-header" style="width:100%;">
+              <div class="opener-left-menu is-open">
+                <span class="top"></span>
+                <span class="middle"></span>
+                <span class="bottom"></span>
+              </div>
+                <a href="home.php" class="navbar-brand"> 
+                 <b>iFactory</b>
+                </a>
+
+              <ul class="nav navbar-nav search-nav">
+                <li>
+
+                </li>
+              </ul>
+
+
+            </div>
+          </div>
+        </nav>
+      <!-- end: Header -->
+
+      <div class="container-fluid mimin-wrapper">
+  
+<?php include('bar.php'); ?>
+
+  		
+          <!-- start: content -->
+            <div id="content">
+                <div class="panel">
+                  <div class="panel-body">
+                      <div class="col-md-6 col-sm-12">
+                        <h3 class="animated fadeInLeft">Optimize Service</h3>
+                        
+                    </div>
+                  </div>                    
+                </div>
+
+                <div class="col-md-12" style="padding:20px;">
+                         <div class="col-md-12 padding-2">
+						        <div class="col-md-4">
+                                    <div class="panel box-v1">
+                                      <div class="panel-heading bg-white border-none">
+                                        <div class="col-md-6 col-sm-6 col-xs-6 text-left padding-0">
+                                          <h4 class="text-left">Optimize Plan</h4>
+                                        </div>
+                                        <div class="col-md-6 col-sm-6 col-xs-6 text-right">
+                                           <h4>
+                                           <span class="fa-newspaper-o fa text-right"></span>
+                                           </h4>
+                                        </div>
+                                      </div>
+                                      <div class="panel-body text-center">
+                                        <h1>0</h1>
+                                        <p>Plan</p>
+                                        <hr/>
+                                      </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="panel box-v1">
+                                      <div class="panel-heading bg-white border-none">
+                                        <div class="col-md-6 col-sm-6 col-xs-6 text-left padding-0">
+                                          <h4 class="text-left">Worker</h4>
+                                        </div>
+                                        <div class="col-md-6 col-sm-6 col-xs-6 text-right">
+                                           <h4>
+                                           <span class="fa fa-users text-right"></span>
+                                           </h4>
+                                        </div>
+                                      </div>
+                                      <div class="panel-body text-center">
+                                        <h1>0</h1>
+                                        <p>Persons</p>
+                                        <hr/>
+                                      </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="panel box-v1">
+                                      <div class="panel-heading bg-white border-none">
+                                        <div class="col-md-6 col-sm-6 col-xs-6 text-left padding-0">
+                                          <h4 class="text-left">Product</h4>
+                                        </div>
+                                        <div class="col-md-6 col-sm-6 col-xs-6 text-right">
+                                           <h4>
+                                           <span class="fa fa-shopping-cart text-right"></span>
+                                           </h4>
+                                        </div>
+                                      </div>
+                                      <div class="panel-body text-center">
+                                        <h1>0</h1>
+                                        <p>things</p>
+                                        <hr/>
+                                      </div>
+                                    </div>
+                                </div>
+                            </div>
+						 </div>
+      		  </div>
+          <!-- end: content -->
+
+    
+         
+          
+      </div>
+
+   
+
+    <!-- start: Javascript -->
+    <script src="asset/js/jquery.min.js"></script>
+    <script src="asset/js/jquery.ui.min.js"></script>
+    <script src="asset/js/bootstrap.min.js"></script>
+   
+    
+    <!-- plugins -->
+    <script src="asset/js/plugins/moment.min.js"></script>
+    <script src="asset/js/plugins/fullcalendar.min.js"></script>
+    <script src="asset/js/plugins/jquery.nicescroll.js"></script>
+    <script src="asset/js/plugins/jquery.vmap.min.js"></script>
+    <script src="asset/js/plugins/maps/jquery.vmap.world.js"></script>
+    <script src="asset/js/plugins/jquery.vmap.sampledata.js"></script>
+    <script src="asset/js/plugins/chart.min.js"></script>
+
+
+    <!-- custom -->
+     <script src="asset/js/main.js"></script>
+     <script type="text/javascript">
+
+
+      
+     </script>
+  <!-- end: Javascript -->
+  </body>
+</html>
